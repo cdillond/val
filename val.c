d2109e3c226ae53fc727d39cc29bdbd089bf3058
cdillond <93200981+cdillond@users.noreply.github.com>
2025-02-28 15:29:40 +0000

    added xml output

--- val.c
+++ val.c
@@ -4,28 +4,31 @@
 
 #include <curl/curl.h>
 
-#define HTML_URL "https://validator.w3.org/nu/?out=json"
-#define HTML_MIME "Content-Type: text/html; charset=utf-8"
+#define HTML_JSON_URL "https://validator.w3.org/nu/?out=json"
+#define HTML_XML_URL "https://validator.w3.org/nu/?out=xml"
+#define HTML_CONTENT_TYPE "Content-Type: text/html; charset=utf-8"
+
 /* User agent taken from https://github.com/validator/validator/wiki/Service-%C2%BB-Input-%C2%BB-POST-body*/
 #define USER_AGENT "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
 
-size_t read_callback(char *buffer, size_t size, size_t nitems, void *userdata)
+size_t read_callback(char *buf, size_t size, size_t n_memb, void *in_file)
 {
-  return fread(buffer, size, nitems, (FILE *)userdata);
+  return fread(buf, size, n_memb, (FILE *)in_file);
 }
 
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+size_t write_callback(char *buf, size_t size, size_t n_memb, void *out_file)
 {
-  return fwrite(ptr, size, nmemb, (FILE *)userdata);
+  return fwrite(buf, size, n_memb, (FILE *)out_file);
 }
 
 int main(int argc, char *argv[])
 {
   FILE *in = stdin;
   FILE *out = stdout;
-
+  const char *url = HTML_JSON_URL;
+  extern char *optarg;
   int opt;
-  while ((opt = getopt(argc, argv, "i:o:")) != -1)
+  while ((opt = getopt(argc, argv, "i:o:x")) != -1)
   {
     switch (opt)
     {
@@ -35,23 +38,28 @@ int main(int argc, char *argv[])
     case 'o':
       out = fopen(optarg, "w");
       break;
-
+    case 'x':
+      url = HTML_XML_URL;
+      break;
     default:
-      fprintf(stderr, "Usage: %s [-i in_path] [-o out_path]\n", argv[0]);
+      fprintf(stderr, "Usage: %s [-i in_path] [-o out_path] [-x xml output]\n", argv[0]);
       return 1;
     }
   }
 
   CURL *curl = curl_easy_init();
   if (!curl)
+  {
+    fputs("error: unable to initialize CURL context\n", stderr);
     /* TODO: handle error */
     return 1;
+  }
 
   struct curl_slist *list = NULL;
-  list = curl_slist_append(list, HTML_MIME);
+  list = curl_slist_append(list, HTML_CONTENT_TYPE);
   list = curl_slist_append(list, USER_AGENT);
 
-  curl_easy_setopt(curl, CURLOPT_URL, HTML_URL);
+  curl_easy_setopt(curl, CURLOPT_URL, url);
   curl_easy_setopt(curl, CURLOPT_POST, 1);
   curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
   curl_easy_setopt(curl, CURLOPT_READDATA, (void *)in);
@@ -59,7 +67,9 @@ int main(int argc, char *argv[])
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)out);
 
-  curl_easy_perform(curl);
+  CURLcode status = curl_easy_perform(curl);
+  if (status)
+    fprintf(stderr, "CURL error: response code %d\n", status);
 
   curl_slist_free_all(list);
   curl_easy_cleanup(curl);
@@ -67,5 +77,5 @@ int main(int argc, char *argv[])
   fclose(in);
   fclose(out);
 
-  return 0;
+  return (int)status;
 }
