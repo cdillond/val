b6e10fe97fba148c7a5922758955da39f48266b0
cdillond <93200981+cdillond@users.noreply.github.com>
2025-02-28 23:39:12 +0000

    refactored CSS validation and added documentation:

--- README.md
+++ README.md
@@ -2,12 +2,28 @@
 This is a simple CLI interface for the W3 HTML and CSS validation services. It is not approved by or affiliated with the W3. I wrote it to experiment with C and to make submitting locally stored HTML and CSS files for validation more ergonomic. Please use it responsibly.
 
 ## Building
-This project depends on [libcurl](https://curl.se/libcurl/). To build, just run:
+This application depends on [libcurl](https://curl.se/libcurl/). To build val, make sure libcurl is installed and then run:
 ```bash
 $ cc -o val val.c -lcurl
 ```
 
 ## Usage
+val accepts the following options.
+- `-i`: input file name [arg required, default behavior: read from `stdin`]
+- `-o`: output file name [arg required, default behavior: write to `stdout`]
+- `-c`: validate CSS input [default behavior: validate HTML input]
+- `-x`: format HTML validation response as XML [must not be used in combination with `-c` or `-f`, default behavior: format HTML validation response as JSON]
+- `-f`: format CSS validation response according to the provided arg (see below) [must not be used in combination with `-x`, default behavior: `text/plain`]
+  - 0 = `text/plain`
+  - 1 = `text`
+  - 2 = `text/html`
+  - 3 = `html`
+  - 4 = `application/xhtml+xml`
+  - 5 = `xhtml`
+  - 6 = `application/soap+xml`
+  - 7 = `soap12`
+
+## Examples
 Validate an HTML file named example.html using the default options.
 ```bash
 $ val < example.html
@@ -20,7 +36,7 @@ Validate an HTML file named example.html and save the output, formatted as xml, 
 ```bash
 $ val -i example.html -x -o out.xml
 ```
-Validate a CSS file named example.css and output the response as plain text.
+Validate a CSS file named example.css and output the response in a soap12 format.
 ```bash
 $ val -c -f 6 < example.css
 ```
--- val.c
+++ val.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,55 +6,44 @@
 
 #include <curl/curl.h>
 
+/* needed for HTML validation requests */
 #define HTML_JSON_URL "https://validator.w3.org/nu/?out=json"
 #define HTML_XML_URL "https://validator.w3.org/nu/?out=xml"
 #define HTML_CONTENT_TYPE "Content-Type: text/html; charset=utf-8"
+#define USER_AGENT "html validator client"
 
+/* needed for CSS validation requests */
 #define CSS_URL "https://jigsaw.w3.org/css-validator/validator?"
-#define CSS_QUERY "text="
-#define CSS_CONTENT_TYPE "Content-Type: text/css; charset=utf-8"
-
-/* User agent taken from https://github.com/validator/validator/wiki/Service-%C2%BB-Input-%C2%BB-POST-body*/
-#define USER_AGENT "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36"
+#define CSS_QUERY_PARAM "text="
 
 enum CSS_OUTPUT_FORMAT
 {
+  TEXT_PLAIN,
+  TEXT,
   TEXT_HTML,
   HTML,
   XHTML_XML,
   XHTML,
   SOAP_XML,
   SOAP12,
-  TEXT_PLAIN,
-  TEXT,
-};
-
-static const char *formats[] = {
-    "output=text/html",
-    "output=html",
-    "output=application/xhtml+xml",
-    "output=xhtml",
-    "output=application/soap+xml",
-    "output=soap12",
-    "output=text/plain",
-    "output=text",
 };
 
 size_t read_callback(char *buf, size_t size, size_t n_memb, void *in_file);
-
 size_t write_callback(char *buf, size_t size, size_t n_memb, void *out_file);
-
 int validate_css(CURL *curl, FILE *in, FILE *out, enum CSS_OUTPUT_FORMAT format);
 
 int main(int argc, char *argv[])
 {
+  /* default settings */
   int status = 1;
   FILE *in = stdin;
   FILE *out = stdout;
   const char *url = HTML_JSON_URL;
   const char *content_type = HTML_CONTENT_TYPE;
   int use_css = 0;
-  unsigned int css_format = TEXT_PLAIN;
+  enum CSS_OUTPUT_FORMAT css_format = TEXT_PLAIN;
+
+  /* parse args */
   extern char *optarg;
   int opt;
   while ((opt = getopt(argc, argv, "i:o:f:xc")) != -1)
@@ -64,17 +52,24 @@ int main(int argc, char *argv[])
     {
     case 'i':
       in = fopen(optarg, "r");
+      if (in == NULL)
+      {
+        fprintf(stderr, "unable to open input file %s\n", optarg);
+        perror(NULL);
+        return status;
+      }
       break;
     case 'o':
       out = fopen(optarg, "w");
+      if (out == NULL)
+      {
+        fprintf(stderr, "unable to open output file %s\n", optarg);
+        perror(NULL);
+        return status;
+      }
       break;
     case 'f':
       css_format = atoi(optarg);
-      if (css_format >= (sizeof formats / sizeof *formats))
-      {
-        fprintf(stderr, "invalid CSS response format argument %d\n", css_format);
-        return status;
-      }
       break;
     case 'x':
       url = HTML_XML_URL;
@@ -84,45 +79,45 @@ int main(int argc, char *argv[])
       break;
     default:
       fprintf(stderr, "Usage: %s [-i in_path] [-o out_path] [-x xml HTML response output] [-c use CSS mode] [-f CSS response format]\n", argv[0]);
-      return 1;
+      goto close_io;
     }
   }
 
+  /* initialize CURL context */
   CURL *curl = curl_easy_init();
   if (!curl)
   {
     fputs("error: unable to initialize CURL context\n", stderr);
-    return status;
+    goto close_io;
   }
 
   if (use_css)
+    status = validate_css(curl, in, out, css_format);
+  else
   {
-    status = validate_css(curl, stdin, stdout, css_format);
-    curl_easy_cleanup(curl);
-    fclose(in);
-    fclose(out);
-    return status;
+    /* HTML validation; much simpler than what's required for CSS */
+    struct curl_slist *list = NULL;
+    list = curl_slist_append(list, content_type);
+    list = curl_slist_append(list, USER_AGENT);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_POST, 1);
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+    curl_easy_setopt(curl, CURLOPT_READDATA, (void *)in);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)out);
+
+    status = curl_easy_perform(curl);
+    curl_slist_free_all(list);
   }
 
-  struct curl_slist *list = NULL;
-  list = curl_slist_append(list, content_type);
-  list = curl_slist_append(list, USER_AGENT);
-
-  curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_easy_setopt(curl, CURLOPT_POST, 1);
-  curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
-  curl_easy_setopt(curl, CURLOPT_READDATA, (void *)in);
-  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)out);
-
-  status = curl_easy_perform(curl);
   if (status)
     fprintf(stderr, "CURL error: response code %d\n", status);
 
-  curl_slist_free_all(list);
   curl_easy_cleanup(curl);
 
+close_io:
   fclose(in);
   fclose(out);
 
@@ -139,70 +134,133 @@ size_t write_callback(char *buf, size_t size, size_t n_memb, void *out_file)
   return fwrite(buf, size, n_memb, (FILE *)out_file);
 }
 
-int validate_css(CURL *curl, FILE *in, FILE *out, enum CSS_OUTPUT_FORMAT format)
+CURLUcode CSS_build_URL(CURLU *url, const char *restrict format, const char *restrict query)
 {
-  int status = 0;
 
+  CURLUcode rc = curl_url_set(url, CURLUPART_URL, CSS_URL, 0);
+  if (rc != 0)
+  {
+    fprintf(stderr, "CURL URL error: code %d\n", rc);
+    return rc;
+  }
+
+  rc = curl_url_set(url, CURLUPART_QUERY, query, CURLU_APPENDQUERY | CURLU_URLENCODE);
+  if (rc != 0)
+  {
+    fprintf(stderr, "CURL URL error: code %d\n", rc);
+    return rc;
+  }
+
+  rc = curl_url_set(url, CURLUPART_QUERY, format, CURLU_APPENDQUERY | CURLU_URLENCODE);
+  if (rc != 0)
+    fprintf(stderr, "CURL URL error: code %d\n", rc);
+
+  return rc;
+}
+
+int CSS_http_get(CURL *curl, CURLU *url, FILE *out)
+{
+  /* begin constructing the HTTP request */
+  curl_easy_setopt(curl, CURLOPT_CURLU, url);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
+
+  int status = curl_easy_perform(curl); /* treat this as a regular int */
+  if (status)
+    fprintf(stderr, "CURL error: response code %d\n", status);
+
+  return status;
+}
+
+/* CSS format enum as a string; returned string must not be freed or written to */
+static const char *CSS_format_str(enum CSS_OUTPUT_FORMAT format)
+{
+  static const char *formats[] = {
+      "output=text/plain",
+      "output=text",
+      "output=text/html",
+      "output=html",
+      "output=application/xhtml+xml",
+      "output=xhtml",
+      "output=application/soap+xml",
+      "output=soap12",
+  };
+
+  if (format < 0 || format >= (sizeof formats / sizeof *formats))
+  {
+    fprintf(stderr, "invalid CSS response format argument %d\n", format);
+    return NULL;
+  }
+
+  return formats[format];
+}
+
+/* returns the len of the input stream or 0 on failure */
+size_t CSS_data_len(FILE *in)
+{
   if (fseek(in, 0, SEEK_END) == -1)
   {
     fputs("error: unable to seek input file\n", stderr);
     perror(NULL);
-    return 1;
+    return 0;
   }
 
-  long int end = ftell(in);
-  if (end == -1)
+  long offset = ftell(in);
+  if (offset == -1)
   {
     fputs("error: unable to determine input file size\n", stderr);
     perror(NULL);
-    return 1;
+    return 0;
   }
 
   rewind(in);
 
-  char *data = malloc(sizeof CSS_QUERY - 1 + end + 1);
-  memcpy(data, CSS_QUERY, sizeof CSS_QUERY);
-  size_t n = fread(data + sizeof CSS_QUERY - 1, sizeof *data, end, in);
-  if (n != (size_t)end)
+  return (size_t)offset;
+}
+
+/* validat_css builds an HTTP request that passes the input file data via a URL encoded query
+ and writes the response to out. The return value is 0 on success and nonzero on failure. */
+int validate_css(CURL *curl, FILE *in, FILE *out, enum CSS_OUTPUT_FORMAT format)
+{
+  int status = 1;
+
+  const char *format_str = CSS_format_str(format);
+  if (format_str == NULL)
+    return status;
+
+  size_t data_len = CSS_data_len(in);
+  if (data_len == 0)
+    return status;
+
+  /* allocate query buffer */
+  size_t param_len = sizeof CSS_QUERY_PARAM - 1; /* do not include null terminator here */
+  size_t buf_len = param_len + data_len;
+  char *buf = malloc(buf_len); /* include space for a null terminator */
+
+  /* write query to buf */
+  memcpy(buf, CSS_QUERY_PARAM, param_len);
+  size_t n = fread(buf + param_len, sizeof *buf, data_len, in);
+  if (n != data_len)
   {
     fputs("error reading input file\n", stderr);
     perror(NULL);
     goto free_data;
   }
+  buf[buf_len] = '\0';
 
-  data[sizeof CSS_QUERY - 1 + end] = '\0';
-
+  /* construct URL with query string*/
   CURLU *url = curl_url();
-  assert(url != NULL);
-
-  CURLUcode rc = curl_url_set(url, CURLUPART_URL, CSS_URL, 0);
-  assert(rc == 0);
-
-  rc = curl_url_set(url, CURLUPART_QUERY, data, CURLU_APPENDQUERY | CURLU_URLENCODE);
-  if (rc != 0)
-  {
-    fprintf(stderr, "CURL URL error: code %d\n", rc);
+  CURLUcode rc = CSS_build_URL(url, format_str, buf);
+  if (rc)
     goto cleanup_url;
-  }
 
-  rc = curl_url_set(url, CURLUPART_QUERY, formats[format], CURLU_APPENDQUERY | CURLU_URLENCODE);
-  if (rc != 0)
-  {
-    fprintf(stderr, "CURL URL error: code %d\n", rc);
-    goto cleanup_url;
-  }
-
-  curl_easy_setopt(curl, CURLOPT_CURLU, url);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
-
-  status = curl_easy_perform(curl);
-  if (status)
-    fprintf(stderr, "CURL error: response code %d\n", status);
+  /* send GET request */
+  status = CSS_http_get(curl, url, out);
 
 cleanup_url:
   curl_url_cleanup(url);
 free_data:
-  free(data);
+  free(buf);
+
   return status;
 }
